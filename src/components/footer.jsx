2dcb4947a0dacfb0e24b7891e65bd97994228fe3
tanishirai <tanishirai2604@gmail.com>
2025-01-17 18:53:57 +0530

    Updated Twitter icon

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -81,7 +81,7 @@ function Footer({ isHindi }) {
               <h4>{t.followUs}</h4>
               <div className="social-links">
                 <a href="#"><i className="fab fa-facebook-f"></i></a>
-                <a href="#"><i className="fab fa-twitter"></i></a>
+                <a href="#"><i className="fab fa-x-twitter"></i></a>
                 <a href="#"><i className="fab fa-instagram"></i></a>
                 <a href="#"><i className="fab fa-linkedin-in"></i></a>
               </div>
