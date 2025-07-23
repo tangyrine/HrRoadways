d354da6ae54e56e0863356857087e133af3b4f7c
Tharunkunamalla <kunamallatharun701@gmail.com>
2025-07-23 18:44:32 +0530

    Solved issue and added hook and done some changes in the translation.json

--- libs/languageStorage.js
+++ libs/languageStorage.js
@@ -1,14 +1,14 @@
 export const getStoredLanguage = () => {
-  const storedLanguage = localStorage.getItem('language');
+  const storedLanguage = localStorage.getItem("language");
 
   // If no language is stored, return 'en' as default
   if (!storedLanguage) {
-    return 'en';
+    return "en";
   }
 
   return storedLanguage;
-}
+};
 
 export const setStoredLanguage = (language) => {
-  localStorage.setItem('language', language);
+  localStorage.setItem("language", language);
 };
