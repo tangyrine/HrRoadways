0a44ca0e923fa4be799535fd8d5580d363f57ec0
Ronnit44 <ronnitsagar2022@vitbhopal.ac.in>
2025-07-28 18:43:31 +0530

    replaced old twitter icon with new X icon

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,8 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Bus, MapPin, Clock, Globe, Facebook, Twitter, Instagram, Linkedin, Share } from 'lucide-react';
+import { Bus, MapPin, Clock, Globe, Facebook, Instagram, Linkedin, Share } from 'lucide-react';
 import { useTranslation } from 'react-i18next';
-import '../styles/footer.css'; // Import the CSS file
+import '../styles/footer.css';
+
+const XIcon = ({ className }) => (
+  <svg
+    className={className}
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 1200 1227"
+    fill="currentColor"
+  >
+    <path d="M713.7 545.7 1160.9 0H1052L677.9 483.4 382.7 0H0l465.2 758.2L0 1226.8h108.9l397.5-500.5 
+    309.7 500.5h382.7L713.7 545.7z" />
+  </svg>
+);
+
 
 function Footer() {
   const { t } = useTranslation();
@@ -12,7 +25,6 @@ function Footer() {
     const timeInterval = setInterval(() => {
       setCurrentTime(new Date());
     }, 1000);
-
     return () => clearInterval(timeInterval);
   }, []);
 
@@ -95,7 +107,7 @@ function Footer() {
                 <div className="footer-social-links">
                   {[
                     { Icon: Facebook, color: "social-facebook", href: "#" },
-                    { Icon: Twitter, color: "social-twitter", href: "#" },
+                    { Icon: XIcon, color: "social-twitter", href: "#" },
                     { Icon: Instagram, color: "social-instagram", href: "#" },
                     { Icon: Linkedin, color: "social-linkedin", href: "https://www.linkedin.com/in/nishantrana07/" },
                   ].map(({ Icon, color, href }, idx) => (
