81c522f275f9b6852e171cdc98b3c09fbee78ec0
Nishant Rana <126577697+NishantRana07@users.noreply.github.com>
2025-08-03 00:54:23 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import './index.css'
-import './i18n'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
+import './i18n';
 
-const root = createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
 
 // Register Service Worker
 if ('serviceWorker' in navigator) {
